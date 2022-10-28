5827a824634b1db8dcd8c04a9e65a34ba3d84047
ciclopeXD <99771661+ciclopeXD@users.noreply.github.com>
2022-10-27 19:52:09 -0500

    Add files via upload

---
+++ Actividad-del-alumno-1-C38-V2-main/sketch.js
@@ -0,0 +1,41 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2;
+var cars = [];
+var fuelImg,coinImg,fuelGroup,coinGroup,conoImg,llantaImg,obstaculeGroup;
+
+function preload() {
+  backgroundImage = loadImage("./assets/background.png");
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  track = loadImage("../assets/track.jpg");
+llantaImg = loadImage("../assets/obstacle2.png")
+  conoImg = loadImage("../assets/obstacle1.png")
+  fuelImg = loadImage ("../assets/fuel.png");
+  coinImg = loadImage("../assets/goldCoin.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
